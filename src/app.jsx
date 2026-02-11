3708cca0cbff824db22f8e10468fa62c96909e00
smithril000 <rileysmith1035@icloud.com>
2026-02-11 10:34:16 -0700

    basic header and footer displaying properly weith react

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div>
+        <header>
+                <h1>Bank - A Dice Game</h1>
+                    <nav>
+                        <menu>
+                            <li><button className="button1" type="button"><a href="index.html">Home</a></button></li>
+                            <li><button className="button2" type="button"><a href="scores.html">High Scores</a></button></li>
+                        </menu>
+                    </nav>
+
+            </header>
+
+        <footer>
+                <span>Author: Riley Smith</span>
+                <a href="https://github.com/smithril000/startup">My Github</a>
+        </footer>
+    </div>
+  );
 }
