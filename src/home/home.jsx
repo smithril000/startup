878fde70f4ffc31ac051a10b8f1ac2cbac20116a
smithril000 <rileysmith1035@icloud.com>
2026-02-25 10:42:01 -0700

    login working, and login button takes you to gameplay

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,22 +1,39 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-export function Home() {
-  return (
-    <main>
-            
-            <div id="popupContainer" className="popup-container">
-            <div className="popup-content">
+import {useNavigate} from 'react-router-dom';
 
-                <div>
-                    <input type="text" placeholder="your@email.com" />
+export function Home() {
+    const [userName, setUser] = React.useState(localStorage.getItem('user'))
+    const [text, setText] = React.useState("")
+    const navigate = useNavigate();
+
+    function loginUser(){
+        setUser(text);
+        localStorage.setItem('user', text)
+        navigate('/gameplay')
+    }
+    function textChange(e){
+        setText(e.target.value)
+    }
+
+    return (
+    
+        <main>
+                
+            <div id="popupContainer" className="popup-container">
+                <div className="popup-content">
+
+                    <div>
+                        <input type="text" onChange={textChange} placeholder="your@email.com" />
+                    </div>
+                    <div>
+                        <input type="password" placeholder="password" />
+                    </div>
+                    <button type="submit" onClick={loginUser}>Login</button>
+                    <button type="submit">Create</button>
                 </div>
-                <div>
-                    <input type="password" placeholder="password" />
-                </div>
-                <button type="submit">Login</button>
-                <button type="submit">Create</button>
             </div>
-        </div>
         </main>
   );
 }
